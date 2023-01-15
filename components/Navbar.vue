7068fdbc1ea8613e0c75cebe7a1d5a09f15f711e
Usama-26 <75754522+Usama-26@users.noreply.github.com>
2023-01-16 01:40:55 +0500

    Navbar Fixed

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,9 +1,20 @@
 <template>
-  <header class="text-gray-600 bg-zinc-800 body-font z-10 w-full">
-    <div class="mx-auto flex flex-wrap justify-between p-5 items-center">
-      <NuxtLink class="flex title-font font-medium items-center text-gray-900">
+  <header
+    class="text-gray-600 z-10 w-full"
+    :class="isScrolled ? 'bg-white fixed top-0 p-3' : 'bg-zinc-800 p-5'"
+  >
+    <div class="mx-auto flex flex-wrap justify-between items-center">
+      <NuxtLink to="/">
         <img
+          v-if="!isScrolled"
           src="~/assets/images/ads-tracking-logo.svg"
+          alt="Ads Tracking"
+          class="w-48"
+          :src="{ '~/assets/images/ads-tracking-logo-white.svg': isScrolled }"
+        />
+        <img
+          v-else
+          src="~/assets/images/ads-tracking-logo-white.svg"
           alt="Ads Tracking"
           class="w-48"
         />
@@ -13,16 +24,35 @@
         <nav
           class="md:ml-auto flex flex-wrap items-center text-base justify-center"
         >
-          <NuxtLink to="/" class="desktop-link">HOME</NuxtLink>
-          <NuxtLink to="/services" class="desktop-link"
+          <NuxtLink
+            to="/"
+            class="desktop-link"
+            :class="isScrolled ? 'hover:text-gray-900' : 'hover:text-gray-100'"
+            >HOME</NuxtLink
+          >
+          <NuxtLink
+            to="/services"
+            class="desktop-link"
+            :class="isScrolled ? 'hover:text-gray-900' : 'hover:text-gray-100'"
             >DIENSTLEISTUNGEN</NuxtLink
           >
-          <NuxtLink to="/customers" class="desktop-link">KUNDEN</NuxtLink>
-          <NuxtLink to="/about" class="desktop-link">ÜBER UNS</NuxtLink>
+          <NuxtLink
+            to="/customers"
+            class="desktop-link"
+            :class="isScrolled ? 'hover:text-gray-900' : 'hover:text-gray-100'"
+            >KUNDEN</NuxtLink
+          >
+          <NuxtLink
+            to="/about"
+            class="desktop-link"
+            :class="isScrolled ? 'hover:text-gray-900' : 'hover:text-gray-100'"
+            >ÜBER UNS</NuxtLink
+          >
         </nav>
         <NuxtLink
           to="/contact"
-          class="inline-flex items-center transition duration-200 py-2 px-6 focus:outline-none border text-gray-100 hover:bg-green-500 hover:border-green-500 uppercase font-bold rounded-full text-base mt-4 md:mt-0"
+          class="inline-flex items-center transition duration-200 py-2 px-6 focus:outline-none border hover:bg-green-600 hover:border-green-500 uppercase font-bold rounded-full text-base mt-4 md:mt-0"
+          :class="isScrolled ? 'text-gray-100 bg-green-500 ' : 'text-gray-100'"
         >
           KONTAKT
         </NuxtLink>
@@ -33,7 +63,11 @@
         @click="isToggled = !isToggled"
       >
         <XMarkIcon v-if="isToggled" class="w-8 h-8 stroke-white" />
-        <Bars3Icon v-else class="w-8 h-8 lg:stroke-green-500 stroke-white" />
+        <Bars3Icon
+          v-else
+          class="w-8 h-8 lg:stroke-green-500 stroke-white"
+          :class="isScrolled ? 'stroke-gray-900' : 'stroke-green-600'"
+        />
       </button>
       <!-- Mobile Nav -->
       <div
@@ -81,10 +115,24 @@ import { XMarkIcon } from "@heroicons/vue/24/outline/index.js";
 
 export default {
   components: { Bars3Icon, XMarkIcon },
+
   data() {
     return {
+      isScrolled: false,
       isToggled: false,
     };
+  },
+  mounted() {
+    window.addEventListener("scroll", this.handleScroll);
+  },
+  methods: {
+    handleScroll() {
+      if (window.scrollY > 50) {
+        this.isScrolled = true;
+      } else {
+        this.isScrolled = false;
+      }
+    },
   },
 };
 </script>
